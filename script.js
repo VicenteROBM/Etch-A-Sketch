45ea8584003c90e2297fb1defc1b19e7e1d47a5e
Vicente <vicente.bravo.rom@gmail.com>
2023-01-11 20:04:48 +0100

    i need to find a way to make it so when the mouse goes over each div that makes up the grid it changes background color and i have no idea how to do it

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 
 
-/* function takes row and columns
-
-for loop that makes columns (add display flex on the colums)
-and another loop that add "columns" value ass grid squares
-
-rows must == columns
-maximum 100 x 100
-*/
+let container = document.getElementById("container")
 
 
-const container = document.getElementById("container")
-
-function makeGrid (row,column){
-
-// for loop creates rows
-for( let i = 0; i< row; i++){
-    let row = document.createElement("div")
-    row.classList.add("row")
-    container.appendChild(row)
-    for(let p=0; p<column; p++){
-        let gridSquare = document.createElement("div")
-        gridSquare.classList.add("gridSquare")
-        row.appendChild(gridSquare)
-    }
-
+//this function creates the grid
+function makeGrid (rows,columns){
+    // this loop creates the row using flexbox inside the conatiner div
+    for(let i=0; i<rows; i++){
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        // this for loop creates the squares and gives them 
+        // the percet size to adapt to container
+        for(let p=0; p<columns; p++){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+            let squareHeight = 800/rows;
+            square.style.height = ""+squareHeight+"px";
+            let squareWidth = 970/columns;
+            square.style.width = ""+squareWidth+"px";  
+            
+        }   
+    }  
+ 
 }
 
 
-    // for loop add squareGrids to each row
-    gridSquare
+// now i need a function that makes it so when the mouse hovers over a div
+// it changes the color to black
 
 
-}
-makeGrid(16,16)
+makeGrid(100,100)
